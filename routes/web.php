3218bef617854348e367d50e587beb26a775d896
rachmkurnia <150273907+rachmkurnia@users.noreply.github.com>
2025-12-14 19:37:41 +0700

    nambahi login page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('modules/dashboard/dashboard');
 });
+Route::get('/login', function () {
+    return view('modules/login/login');
+})->name('login');
